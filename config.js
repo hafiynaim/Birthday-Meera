d9d8237265119141fc6b430a15c80f93c77b3c1d
HafiyNaim <muhdhafiynaim@gmail.com>
2024-12-27 19:18:29 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+const CONFIG = {
+    titleWeb: "HBD MEERA",
+    introTitle: 'HAPPY BIRTHDAY NUR AMEERA ELSA!',
+    introDesc: `Buka untuk surprise`,
+    btnIntro: '^^HiHi^^',
+    title: 'Betul ke harini birthday meera??',
+    desc: 'Kalau betul tekan yeee kalau tak tekan tidakk',
+    btnYes: 'YEEEE',
+    btnNo: 'TIDAKK',
+    question: 'hehehe macam mana perasaan meera harini? Enjoy your birthday Meeraa!',
+    btnReply: 'Takyah tulis pape pon takpe',
+        mess: 'Nanti kalau ada kesempatan kita sambut birthday sekali meera :D',
+    btnAccept: 'Babaiii',
+    messLink: '' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
